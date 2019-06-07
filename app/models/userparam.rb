a0ab68e12db73bf1bea8b3a6b2c8146413b908e6
Victoria-zavatska95 <viktoriazavacka8@gmail.com>
2019-06-07 10:50:06 +0300

    added actions to userparams controller

--- app/models/userparam.rb
+++ app/models/userparam.rb
@@ -1,2 +1,9 @@
 class Userparam < ApplicationRecord
+	belongs_to :user
+
+	validates :firstname, :lastname, :age, :hometown, :occupation, presence: true
+    validates :age, numericality: { only_integer: true, message: "please enter only integer" }
+    validates :firstname, :lastname, format: { with: /\A[a-zA-Z]+\z/,
+    message: "please enter only letters" }
+    validates :lastname,:firstname, length: { in: 3..20 }
 end
