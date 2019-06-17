359404f95757ae980022a8ffe643e7016a72193a
VictoriaZavatska <viktoriazavacka8@gmail.com>
2019-06-17 21:15:37 +0300

    add route post_no_responder_messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,8 +19,6 @@ def index
 	@messages = current_user.messages.order("id DESC")
 end
 
-def show 
-end
 
 def destroy
 	@message = Message.find(params[:id])
@@ -32,8 +30,18 @@ def send_no_responder_messages
 	@message = Message.new
 end	
 
+def post_no_responder_messages
+	@message = current_user.messages.build(messageparams)
+	@message.from_user_id = current_user.id
+    if @message.save
+    @to_other_user_message = User.find(@message.to_user_id).messages.build(body: @message.body, from_user_id: current_user.id, to_user_id: @message.to_user_id)
+    @to_other_user_message.save
+
+    redirect_to messages_path	
+end	
+end	
 
 private def messageparams 
-	params.require(:message).permit(:body, :user_id)
+	params.require(:message).permit(:body, :user_id, :to_user_id, :from_user_id)
 	end
 end
